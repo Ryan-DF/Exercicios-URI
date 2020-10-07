64ab2f185cfad4010fbbf6ad161689117bbc39bb
Ryan-DF <68028256+Ryan-DF@users.noreply.github.com>
2020-10-07 15:53:16 -0300

    Add files via upload

---
+++ 1015.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double x1, x2, y1, y2, distancia;
+
+    scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+
+    distancia = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+
+    printf("%.4lf", distancia);
+
+
+    return 0;
+}
