a7e3ad3460726c9dc16c87438cd429228bf47ae6
Ryan-DF <68028256+Ryan-DF@users.noreply.github.com>
2020-10-07 17:01:37 -0300

    Add files via upload

---
+++ 1016.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int kilometros,tempo;
+    scanf("%d", &kilometros);
+
+    tempo = kilometros*2;
+
+    printf("%d minutos\n", tempo);
+
+    return 0;
+}
